ed22e7c5cddb41eac6eacbd5c8fef1944a8c38f7
chichunchen <chichunchen844@gmail.com>
2017-09-14 13:48:37 -0400

    Passing 2X more tests.

--- lib/chichun_enum.rb
+++ lib/chichun_enum.rb
@@ -41,6 +41,29 @@ module ChiChunEnumerable
     
   end
 
+  def chunk_while
+  end
+
+  def collect &block
+    if block_given?
+      result = []
+      each do |element|
+        result << block.call(element)  
+      end
+      result
+    else
+      to_enum(:each)
+    end
+  end
+
+  def collect_concat &block
+    result = []
+    each do |element|
+      result.concat(block.call(element)) 
+    end
+    result
+  end
+
   def find(ifnone=nil, &block)
     if block_given?
       result = nil
@@ -133,24 +156,145 @@ module ChiChunEnumerable
     result
   end
 
-  def reduce(accumulator, operation=nil, &block)
+  def max(count=nil, &block)
+    if block_given?
+      if count.nil?
+        reduce do |accumulator, element|
+          accumulator = block.call(element) == 1 ? element : accumulator
+        end
+      else
+        ## call sort...
+      end
+    else
+      if count.nil?
+        reduce do |accumulator, element|
+          accumulator < element ? element : accumulator
+        end
+      else
+        result = []
+        # call sort...
+      end  
+    end
+  end
+
+  def reduce(accumulator = nil, operation = nil, &block)
+    if accumulator.nil? && operation.nil? && block.nil?
+      raise ArgumentError, "you must provide an operation or a block"
+    end
+
     if operation && block
       raise ArgumentError, "you must provide either an operation symbol or a block, not both"
     end
 
+    if operation.nil? && block.nil?
+      operation = accumulator
+      accumulator = nil
+    end
+
+    # when operation is given by the client, assign the operation by using `send`
+    # send use the first argument as the method you want to call, so we defer the choose of the function
     block = case operation
       when Symbol
-        lambda { |acc,value| acc.send(operation, value) }
+        lambda { |acc, value| acc.send(operation, value) }
       when nil
         block
       else
-        raise ArgumentError, "the operation provided must be a symbol"
+      raise ArgumentError, "the operation provided must be a symbol"
     end
 
+    if accumulator.nil?
+      ignore_first = true
+      accumulator = first
+    end
+
+    index = 0
+
     each do |element|
-      accumulator = block.call(accumulator, element)
+      unless ignore_first && index == 0
+        accumulator = block.call(accumulator, element)
+      end
+      index += 1
     end
     accumulator
+  end 
+
+  def sort &block
+    result = []
+    if block_given?
+    else
+      # if block not given, use descending order
+
+    end
+  end
+
+  def sort_by &block
+  end
+
+  def sum init=nil, &block
+    if block_given?
+      if init.nil?
+        init = block.call(first)
+        result = reduce(init) do |accumulator, element|
+          accumulator + block.call(element)
+        end
+        result - init
+      else
+        reduce(init) do |accumulator, element|
+          accumulator + block.call(element)
+        end
+      end
+    else
+      # no given block
+      reduce(init) do |accumulator, element|
+        accumulator + element
+      end
+    end
+  end
+
+  def take count=0
+    result = []
+    each do |element|
+      if (count == 0)
+        break
+      else
+        count = count-1
+      end
+      result << element  
+    end
+    result
+  end
+
+  # returns an array of prior element until the block returns nil or false
+  def take_while &block
+    if block_given?
+      result = []
+      each do |element|
+        if block.call(element) != nil and block.call(element) != false
+          result << element
+        else
+          break
+        end
+      end
+      result
+    else
+      to_enum(:each)
+    end
+  end
+
+  def to_a
+    result = []
+    each do |element|
+      result << element
+    end
+    result
+  end
+
+  def to_h
+    result = {}
+    each do |key, value|
+      result[key] = value
+    end
+    result
   end
 end
 
